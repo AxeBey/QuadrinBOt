8d9168e3b2d5131436ab9892fa8e09c79492c53f
AxeBey <48212968+AxeBey@users.noreply.github.com>
2019-03-07 18:38:08 +0300

    Add files via upload

---
+++ komutlar/ping.js
@@ -0,0 +1,17 @@
+﻿const Discord = require('discord.js');
+exports.run = function(client, message, args) {
+  message.reply('Merhaba! **' + client.ping + '** ms');
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'Merhaba',
+  description: 'Botla selamlaşmanızı sağlar !',
+  usage: 'merhaba'
+};
